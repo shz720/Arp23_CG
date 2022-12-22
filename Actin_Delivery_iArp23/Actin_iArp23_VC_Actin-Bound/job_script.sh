c218c5ce5b7fd82793cc0358993968116bbd592c
shz720 <121265520+shz720@users.noreply.github.com>
2022-12-22 18:14:14 -0500

    Add files via upload

---
+++ Actin_Delivery_iArp23/Actin_iArp23_VC_Actin-Bound/job_script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --constraint="lustre"
+#SBATCH -p shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=28
+#SBATCH -t 48:00:00
+#SBATCH -J wasp_actin_arp23
+#SBATCH -A TG-MCB180021 
+#SBATCH -o arp23_filament.%j.%N.out
+#SBATCH -e arp23_filament.%j.%N.err
+#SBATCH --export=ALL
+
+module purge
+module load gpu/0.15.4
+module load intel/19.0.5.281
+module load intel-mpi/2019.8.254
+module load slurm
+
+srun --mpi=pmi2 -n 28 --cpus-per-task=1 /home/szhang94/SRC/lammps-29Oct20/build/lmp_mpi -p 28x1 -in run.lmp
